1d6f0995621b987c28f52375c5ad8f81c1ceafb6
westbrown687 <ojiakofoe@gmail.com>
2026-07-24 06:27:36 +0100

    Dove-app-2

--- Todolist-App/src/HomePage.jsx
+++ Todolist-App/src/HomePage.jsx
@@ -4,19 +4,70 @@ export function HomePage() {
   return (
     <>
       <title>Dove App</title>
-
-      <div className="todo-container">
-        <div className="Img-container">
-          <button>
-            <div>Menu</div>
-            <img src="/images/navigation-button.svg" alt="" />
-          </button>
+      <div className="app-container">
+        <div className="side-bar">
+          <div className="todo-menu">
+            <div className="menu-text">Menu</div>
+            <button className="menu-button">
+              <img
+                className="menuimg"
+                src="/images/navigation-button.svg"
+                alt="Navigation"
+              />
+            </button>
+          </div>
+          <input type="text" />
         </div>
 
-        <div className="Date-con">
-          <div className="Today">Today</div>
-          <div className="Date">5</div>
-        </div>
+        <main className="body-content">
+          <section className="date-con">
+            <div className="today">Today</div>
+            <div className="date">
+              <button className="date-button">5</button>
+            </div>
+          </section>
+
+          <section className="tasks-section">
+            <button className="newtask-con">
+              <img className="plus-img" src="/images/plus-alt.svg" alt="Add" />
+              <span>Add New task</span>
+            </button>
+
+            <div className="activities-container">
+              <div className="activity-item">
+                <input type="checkbox" />
+                <span>Research Content Ideas</span>
+              </div>
+
+              <div className="activity-item">
+                <input type="checkbox" />
+                <span>Create a database of guest authors</span>
+              </div>
+
+              <div className="activity-box">
+                <div>
+                  <input type="checkbox" />
+                  <span>Renew driver's license</span>
+                </div>
+
+                <button className="trash-button">
+                  <img className="trash" src="/images/trash.svg" alt="Delete" />
+                  <span>22-08-26</span>
+                </button>
+              </div>
+
+              <div className="activity-item">
+                <input type="checkbox" />
+                <span>Consult accountant</span>
+              </div>
+
+              <div className="activity-item">
+                <input type="checkbox" />
+                <span>Print business card</span>
+              </div>
+            </div>
+          </section>
+        </main>
       </div>
     </>
   );
