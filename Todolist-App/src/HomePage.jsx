bf9f9cf2ca1c31fa6851467d6a424bcfd5379e9d
westbrown687 <ojiakofoe@gmail.com>
2026-07-26 09:46:52 +0100

    Dove-app-3

--- Todolist-App/src/HomePage.jsx
+++ Todolist-App/src/HomePage.jsx
@@ -1,11 +1,15 @@
 import "./homepage.css";
+import { FaSearch } from "react-icons/fa";
+import { FaChevronLeft, FaChevronRight, FaChevronDown } from "react-icons/fa";
+import { HiBars3 } from "react-icons/hi2";
+import { HiCalendar } from "react-icons/hi";
 
 export function HomePage() {
   return (
     <>
       <title>Dove App</title>
       <div className="app-container">
-        <div className="side-bar">
+        <aside className="side-bar">
           <div className="todo-menu">
             <div className="menu-text">Menu</div>
             <button className="menu-button">
@@ -16,8 +20,31 @@ export function HomePage() {
               />
             </button>
           </div>
-          <input type="text" />
-        </div>
+
+          <div className="search-container">
+            <FaSearch className="search-icon" />
+            <input className="search" type="text" placeholder="Search" />
+          </div>
+          <div>
+            <div className="tasks">TASK</div>
+            <div>
+              <div className="upcoming-con">
+                <FaChevronRight className="chevron-right" />
+                <button className="upcoming-button">Upcoming</button>
+                <button className="upcoming-digit">12</button>
+              </div>
+              <div className="today-con">
+                <HiBars3 className="hibar" />
+                <button className="today-button">Today</button>
+                <button className="today-digit">5</button>
+              </div>
+              <div className="calender-con">
+                <HiCalendar className="calender" />
+                <button className="calender-button">Calender</button>
+              </div>
+            </div>
+          </div>
+        </aside>
 
         <main className="body-content">
           <section className="date-con">
